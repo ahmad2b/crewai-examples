b506aac38cdc55680164b889f5a5e4ba9f033c6c
Muhammad Ahmad <ahmadshaukat_4@outlook.com>
2024-06-14 07:26:02 +0500

    feat: Add customer support automation agents and tasks

--- 02_customer_support_automation/app/main.py
+++ 02_customer_support_automation/app/main.py
@@ -1,11 +1,32 @@
 from fastapi import FastAPI
+from crewai import Crew
+
+from app.agents import support_agent, support_quality_assurance_agent
+from app.tasks import inquiry_resolution, quality_assurance_review
+
 
 app = FastAPI(
     title="Customer Support Automation CrewAI",
     version="0.0.1",
     )
 
+crew = Crew(
+  agents=[support_agent, support_quality_assurance_agent],
+  tasks=[inquiry_resolution, quality_assurance_review],
+  verbose=2,
+  memory=True
+)
+
 @app.get("/")
 def read_root():
     return {"Hello": "Crew AI"}
 
+@app.post("/inquiry_resolution")
+def inquiry_resolution_endpoint(customer: str, person:str, inquiry: str):
+    inputs = {
+    "customer": customer,
+    "person": person,
+    "inquiry": inquiry
+    }
+    result = crew.kickoff(inputs=inputs)
+    return {"result": result}
